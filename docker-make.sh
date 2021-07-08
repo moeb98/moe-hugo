a006ad2100a4074bfa0e94b718407aa0e7c7629d
moeb98 <55314997+moeb98@users.noreply.github.com>
2021-07-08 10:58:11 +0200

    Update docker-make.sh

--- docker-make.sh
+++ docker-make.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-RNAME=mrmoe/moe-hugo
+REPO=mrmoe/moe-hugo
 VER=0.84.4
-BASENAME=alpine:latest
+BASEIMAGE=alpine:latest
 TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
-echo $TAG
+
 case "$1" in
 	"build")
 		docker buildx build \
 	      --platform linux/amd64,linux/arm/v7,linux/arm/v6 \
-  	      -t $RNAME:latest -t $RNAME:$TAG --push \
+  	      -t $REPO:latest -t $REPO:$TAG --push \
 	      --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 	      --build-arg VCS_REF=`git rev-parse --short HEAD` \
-	      --build-arg BASEIMAGE=$BASENAME \
+	      --build-arg BASEIMAGE=$BASEIMAGE \
 	      --build-arg VERSION=$VER .
 		;;
 	"console")
-		docker run -it --rm --entrypoint "/bin/ash" $RNAME:$TAG
+		docker run -it --rm --entrypoint "/bin/ash" $REPO:$TAG
 		;;
 	* )
 		cat << EOF
